4050380df6de8a9ef07d4783e99985f5076bd7f8
Ruhshan <ruhshan.ahmed@gmail.com>
2021-03-03 20:08:39 +0600

    Setup initial structure

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <sample-form></sample-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import SampleForm from '@/components/SampleForm.vue'
+
+@Component({
+  components: { SampleForm }
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
+}
+</style>
---
+++ src/components/SampleForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <b-container>
+      <b-form>
+        <b-form-group
+          id="input-group-1"
+          label="Email address:"
+          label-for="input-1"
+          description="We'll never share your email with anyone else."
+        >
+          <b-form-input
+            id="input-1"
+            type="email"
+            placeholder="Enter email"
+            required
+          ></b-form-input>
+        </b-form-group>
+      </b-form>
+    </b-container>
+  </div>
+</template>
+
+<script lang="ts">
+
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({ components: {} })
+export default class SampleForm extends Vue {
+}
+
+</script>
